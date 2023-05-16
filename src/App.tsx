74cfaf64b99be66e90c17997fa09ea2c72463ebd
ArtyomKub <coder.kub.lover@gmail.com>
2023-05-17 01:15:11 +0200

    Add a new tag (6-end)

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className={'App'}>
-            <ul>
-                <li>CSS</li>
-                <li>HTML</li>
-                <li>JS</li>
-                <li>React</li>
-            </ul>
+        <div>
+            <div>one div</div>
+            <div className={'App'}>
+                <ul>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>JS</li>
+                    <li>React</li>
+                </ul>
+            </div>
         </div>
     )
 }
-
 
 
 export default App;
